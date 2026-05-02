8c1b8b115904db31680b77d0eff6d715254fce9e
Marvellous Obatale <75810831+MARVELGARR@users.noreply.github.com>
2026-05-02 22:48:12 +0100

    still adding

--- app/settings/upstach.qstach.config.ts
+++ app/settings/upstach.qstach.config.ts
@@ -1,9 +1,15 @@
 
 
 
-import { Client } from "@upstash/qstash";
+import { Client, Receiver } from "@upstash/qstash";
 
 // Token is read from QSTASH_TOKEN env var (set in .env)
 export const client = new Client({
     token: process.env.QSTASH_TOKEN!,
+    
 });
+
+export const reciever = new Receiver({
+    currentSigningKey: process.env.QSTASH_CURRENT_SIGNING_KEY,
+    nextSigningKey: process.env.QSTASH_NEXT_SIGNING_KEY
+})
